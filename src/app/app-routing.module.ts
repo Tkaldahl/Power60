84cd9633c8dfc6b7eaeaf2dbfd54278143240e9d
tkaldahl <tkaldahl90@gmail.com>
2022-12-23 11:00:10 -0500

    move create-account into its own dir

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CreateAccountPage } from './accounts-plugin/pages/create-account.page';
+import { CreateAccountPage } from './accounts-plugin/pages/create-account/create-account.page';
 
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CreateAccountPage } from './accounts-plugin/pages/create-account.page';
+import { CreateAccountPage } from './accounts-plugin/pages/create-account/create-account.page';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
